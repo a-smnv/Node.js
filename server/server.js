548e7ab505a79e0223f1528f9a0a21e4d7f846ff
Anastasia Semenova <Anastasia_Semenova@epam.com>
2021-01-18 16:48:36 +0300

    node.js lesson3

--- server/server.js
+++ server/server.js
@@ -1,40 +1,53 @@
-const http = require("http");
-// const url = require("url");
-let names = [];
-const port = 8080;
-const fs = require("fs");
-const dbName = "names.json";
+const express = require("express");
+// const fs = require("fs");
+// const dbName = "names.json";
+// const names = [];
+const app = express();
+const bodyParser = require("body-parser");
 
-const requestHandler = (request, response) => {
-  names = JSON.parse(fs.readFileSync(dbName, "utf8"));
+app.use(bodyParser.json());
 
-  if (request.method === "POST") {
-    if (request.headers.iknowyoursecret === "TheOwlsAreNotWhatTheySeem") {
-      names.push({
-        name: request.headers.username,
-        ip: request.connection.remoteAddress,
-      });
-      fs.writeFile(dbName, JSON.stringify(names), (err) => {
-        if (err) {
-          throw err;
-        }
-      });
-      response.end(
-        names.length
-          ? `Hello, ${names.map((user) => `${user.name}`).join(", ")}`
-          : "Hello"
-      );
-    } else {
-      console.log("You don't know the secret!");
-    }
+app.use((request, response, next) => {
+  if (request.method === "GET") {
+    console.log("I see you are using wrong method");
+  } else if (request.method === "POST") {
+    console.log("You are using right method");
   }
-};
-
-const server = http.createServer(requestHandler);
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log("The exception happened:", err);
-  }
-  console.log(`Server listening on ${port}`);
+  console.log(">>>", request.body);
+  next();
 });
+
+app.post("/", (request, response, next) => {
+  console.log("The route / has been detected");
+  response.end("Alright");
+});
+
+app.post("/specific", (request, response, next) => {
+  console.log("The route /specific has been detected");
+  response.end("Alright");
+});
+//   names = JSON.parse(fs.readFileSync(dbName, "utf8"));
+
+//   if (request.method === "POST") {
+//     if (request.headers.iknowyoursecret === "TheOwlsAreNotWhatTheySeem") {
+//       names.push({
+//         name: request.headers.username,
+//         ip: request.connection.remoteAddress,
+//       });
+//       fs.writeFile(dbName, JSON.stringify(names), (err) => {
+//         if (err) {
+//           throw err;
+//         }
+//       });
+//       response.end(
+//         names.length
+//           ? `Hello, ${names.map((user) => `${user.name}`).join(", ")}`
+//           : "Hello"
+//       );
+//     } else {
+//       console.log("You don't know the secret!");
+//     }
+//   }
+// };
+
+app.listen(8080, console.log(`Server listening on 8080`));
